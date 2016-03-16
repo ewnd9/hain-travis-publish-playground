15887a2e1712592a7014f12b8dab5b70d5bc0ce9
Heejin Lee <monster@teamapptizer.com>
2016-03-16 23:48:13 +0900

    Updated gulpfile, package.json to be able to make executables

--- gulpfile.js
+++ gulpfile.js
@@ -4,29 +4,55 @@ const gulp = require('gulp');
 const gutil = require('gulp-util');
 const babel = require('gulp-babel');
 const sourcemaps = require('gulp-sourcemaps');
-const shell = require('gulp-shell');
+const packager = require('electron-packager');
+const path = require('path');
+const install = require('gulp-install');
+const merge = require('merge-stream');
 
 const webpack = require('webpack');
 
 gulp.task('main', () => {
-  gulp.src('./app/main-es6/**/*.js')
+  const js = gulp.src('./app/main-es6/**/*.js')
     .pipe(sourcemaps.init())
     .pipe(babel({
       presets: ['es2015', 'react']
     }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./app/main'));
-  gulp.src('./app/main-es6/**/*.json')
+  const json = gulp.src('./app/main-es6/**/*.json')
     .pipe(gulp.dest('./app/main'));
+  return merge(js, json);
 });
 
-gulp.task('renderer', (complete) => {
+gulp.task('renderer', (done) => {
   webpack(require('./webpack.config.js'), (err, stats) => {
     if (err) {
       throw new gutil.PluginError('webpack', err);
     }
     gutil.log('webpack', stats.toString());
-    complete();
+    done();
+  });
+});
+
+gulp.task('deps', () => {
+  return gulp.src('./app/package.json')
+    .pipe(install({ production: true }));
+});
+
+gulp.task('build', ['main', 'renderer', 'deps'], (done) => {
+  packager({
+    arch: 'x64',
+    dir: path.join(__dirname, 'app'),
+    platform: 'win32',
+    asar: true,
+    overwrite: true,
+    icon: path.join(__dirname, 'build', 'icon.ico')
+  }, (err, appPath) => {
+    if (err) {
+      console.log(err);
+      return done(err);
+    }
+    return done();
   });
 });
 
