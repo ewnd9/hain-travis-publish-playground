1208c3ecd084cb6e1842a3533ee8167657c88338
Heejin Lee <monster@teamapptizer.com>
2016-03-16 22:59:42 +0900

    Fixed firstlaunch to write _launched_ file in first launch only

--- main-es6/app/firstlaunch.js
+++ main-es6/app/firstlaunch.js
@@ -1,12 +1,14 @@
 'use strict';
 
 const fs = require('fs');
-const _isFirstLaunch = fs.existsSync('./_launched_');
+const _isFirstLaunch = !fs.existsSync('./_launched_');
 
 function isFirstLaunch() {
-  return !_isFirstLaunch;
+  return _isFirstLaunch;
 }
 
-fs.writeFileSync('./_launched_', 'HELLO');
+if (_isFirstLaunch) {
+  fs.writeFileSync('./_launched_', 'HELLO');
+}
 
 module.exports = { isFirstLaunch };
