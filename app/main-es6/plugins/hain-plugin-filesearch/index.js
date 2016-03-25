0f2163579da10fd87786613be5055684cdb70cd5
Heejin <monster@teamappetizer.com>
2016-03-25 19:56:19 +0900

    Added support for searching folders, etc

--- app/main-es6/plugins/hain-plugin-filesearch/index.js
+++ app/main-es6/plugins/hain-plugin-filesearch/index.js
@@ -8,9 +8,8 @@ const path = require('path');
 
 const matchFunc = (filePath, stats) => {
   const ext = path.extname(filePath).toLowerCase();
-  if (!stats.isFile()) {
-    return false;
-  }
+  if (stats.isDirectory())
+    return true;
   return (ext === '.exe' || ext === '.lnk');
 };
 
@@ -46,41 +45,6 @@ module.exports = (context) => {
     }
   }
 
-  function startup() {
-    co(function* () {
-      yield* refreshIndex(recursiveSearchDirs, true);
-      yield* refreshIndex(flatSearchDirs, false);
-      yield* setupWatchers(recursiveSearchDirs, true);
-      yield* setupWatchers(flatSearchDirs, false);
-    }).catch((err) => {
-      logger.log(err);
-      logger.log(err.stack);
-    });
-  }
-
-  function search(query, res) {
-    const query_trim = query.replace(' ', '');
-    const searched = matchutil.fuzzy(db, query_trim, (x) => x);
-    const result = searched.slice(0, 20).map((x) => {
-      const filePath = x.elem;
-      const filePath_bold = matchutil.makeStringBoldHtml(filePath, x.matches);
-      const filePath_base64 = new Buffer(filePath).toString('base64');
-      return {
-        id: filePath,
-        title: path.basename(filePath, path.extname(filePath)),
-        desc: filePath_bold,
-        icon: `icon://${filePath_base64}`
-      };
-    });
-    res.add(result);
-  }
-
-  function execute(id, payload) {
-    logger.log(`${id} executed`);
-    shell.openItem(id);
-    app.close();
-  }
-
   function lazyRefreshIndex(dir, recursive) {
     const _lazyKey = lazyIndexingKeys[dir];
     if (_lazyKey !== undefined) {
@@ -102,6 +66,55 @@ module.exports = (context) => {
         lazyRefreshIndex(_dir, recursive);
       });
     }
+  }
+
+  function startup() {
+    co(function* () {
+      yield* refreshIndex(recursiveSearchDirs, true);
+      yield* refreshIndex(flatSearchDirs, false);
+      yield* setupWatchers(recursiveSearchDirs, true);
+      yield* setupWatchers(flatSearchDirs, false);
+    }).catch((err) => {
+      logger.log(err);
+      logger.log(err.stack);
+    });
+  }
+
+  function computeRatio(filePath) {
+    let ratio = 1;
+    const ext = path.extname(filePath).toLowerCase();
+    const basename = path.basename(filePath).toLowerCase();
+    if (ext !== '.lnk' && ext !== '.exe')
+      ratio *= 0.5;
+    if (basename.indexOf('uninstall') >= 0 || basename.indexOf('remove') >= 0)
+      ratio *= 0.9;
+    return ratio;
+  }
+
+  function search(query, res) {
+    const query_trim = query.replace(' ', '');
+    const searched = matchutil.fuzzy(db, query_trim, (x) => x);
+    const result = searched.slice(0, 10).map((x) => {
+      const filePath = x.elem;
+      const filePath_bold = matchutil.makeStringBoldHtml(filePath, x.matches);
+      const filePath_base64 = new Buffer(filePath).toString('base64');
+      const score = x.score * computeRatio(filePath);
+      return {
+        id: filePath,
+        title: path.basename(filePath, path.extname(filePath)),
+        desc: filePath_bold,
+        icon: `icon://${filePath_base64}`,
+        group: 'Files & Folders',
+        score
+      };
+    });
+    res.add(result);
+  }
+
+  function execute(id, payload) {
+    logger.log(`${id} executed`);
+    shell.openItem(id);
+    app.close();
   }
 
   return { startup, search, execute };
