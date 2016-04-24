b2d7976dae7b271a032a988208cb1ab43946fec1
Heejin <monster@teamappetizer.com>
2016-04-24 22:03:00 +0900

    Update build script (to support 32bit, chocolatey script)

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = 'Hain'
 $installerType = 'exe' # Squirrel Installer / Updater
-$url64 = 'https://github.com/appetizermonster/hain/releases/download/v0.1.0/HainSetup-x64-v0.1.0.exe'
-$url32 = $url64
+$url64 = 'https://github.com/appetizermonster/hain/releases/download/v${version}/HainSetup-x64-v${version}.exe'
+$url32 = 'https://github.com/appetizermonster/hain/releases/download/v${version}/HainSetup-ia32-v${version}.exe'
 $silentArgs = "" # The installation is silent by default
- 
+
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url32" "$url64"
--- gulpfile.js
+++ gulpfile.js
@@ -10,10 +10,9 @@ const zip = require('gulp-zip');
 const electronInstaller = require('electron-winstaller');
 const fs = require('fs');
 const del = require('del');
+const mergeStream = require('merge-stream');
+const replace = require('gulp-replace');
 const appPkg = require('./app/package.json');
-
-// Configuration
-const ARCH = 'x64';
 
 gulp.task('deps', () => {
   return gulp.src('./app/package.json')
@@ -35,54 +34,79 @@ gulp.task('renderer', ['deps', 'clean:renderer'], () => {
   return js;
 });
 
-gulp.task('build', ['renderer', 'deps'], (done) => {
-  packager({
-    arch: ARCH,
-    dir: path.join(__dirname, 'app'),
-    platform: 'win32',
-    asar: true,
-    ignore: /(renderer-jsx|__tests__)/gi,
-    overwrite: true,
-    out: path.join(__dirname, 'out'),
-    icon: path.join(__dirname, 'build', 'icon.ico'),
-    'version-string': {
-      ProductName: 'Hain',
-      CompanyName: 'Heejin Lee'
-    }
-  }, (err, appPath) => {
-    if (err) {
-      console.log(err);
-      return done(err);
-    }
-    return done();
+function build(arch) {
+  return new Promise((resolve, reject) => {
+    packager({
+      arch,
+      dir: path.join(__dirname, 'app'),
+      platform: 'win32',
+      asar: true,
+      ignore: /(renderer-jsx|__tests__)/gi,
+      overwrite: true,
+      out: path.join(__dirname, 'out'),
+      icon: path.join(__dirname, 'build', 'icon.ico'),
+      'version-string': {
+        ProductName: 'Hain',
+        CompanyName: 'Heejin Lee'
+      }
+    }, (err, appPath) => {
+      if (err) {
+        console.log(err);
+        return reject(err);
+      }
+      return resolve();
+    });
   });
-});
+}
 
-gulp.task('build-zip', ['build'], () => {
-  const filename = `Hain-${ARCH}-v${appPkg.version}.zip`;
-  return gulp.src(`./out/Hain-win32-${ARCH}/**/*`)
+function buildZip(arch) {
+  const filename = `Hain-${arch}-v${appPkg.version}.zip`;
+  return gulp.src(`./out/Hain-win32-${arch}/**/*`)
             .pipe(zip(filename))
             .pipe(gulp.dest('./out/'));
-});
+}
 
-gulp.task('build-installer', ['build'], (done) => {
-  const filename = `HainSetup-${ARCH}-v${appPkg.version}.exe`;
-  electronInstaller.createWindowsInstaller({
-    appDirectory: `./out/Hain-win32-${ARCH}`,
-    outputDirectory: './out',
+function buildInstaller(arch) {
+  const filename = `HainSetup-${arch}-v${appPkg.version}.exe`;
+  return electronInstaller.createWindowsInstaller({
+    appDirectory: path.resolve(`./out/hain-win32-${arch}`),
+    outputDirectory: `./out/${arch}`,
     authors: 'Heejin Lee',
     title: 'Hain',
     iconUrl: 'https://raw.githubusercontent.com/appetizermonster/Hain/master/build/icon.ico',
     setupIcon: path.resolve('./build/icon.ico'),
     loadingGif: path.resolve('./build/installer.gif'),
+    skipUpdateIcon: true, // HACK to resolve rcedit error (electron-winstaller)
     noMsi: true
   }).then(() => {
-    fs.renameSync('./out/Setup.exe', `./out/${filename}`);
-    done();
-  }).catch((err) => done(err));
+    fs.renameSync(`./out/${arch}/Setup.exe`, `./out/${filename}`);
+  });
+}
+
+gulp.task('build', ['renderer', 'deps'], () => {
+  return build('ia32')
+    .then(() => build('x64'));
 });
 
-gulp.task('build-all', ['build-zip', 'build-installer']);
+gulp.task('build-zip', ['build'], () => {
+  return mergeStream(
+    buildZip('ia32'),
+    buildZip('x64')
+  );
+});
+
+gulp.task('build-installer', ['build'], () => {
+  return buildInstaller('ia32')
+    .then(() => buildInstaller('x64'));
+});
+
+gulp.task('build-chocolatey', () => {
+  return gulp.src('./chocolatey/**/*')
+    .pipe(replace('${version}', appPkg.version))
+    .pipe(gulp.dest('./out/chocolatey/'));
+});
+
+gulp.task('build-all', ['build-zip', 'build-installer', 'build-chocolatey']);
 
 gulp.task('watch', ['renderer'], () => {
   const opts = {
